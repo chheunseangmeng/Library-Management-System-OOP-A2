9c9c91b14d483ce0294327a8ca3bba169134758c
sonarykheang123 <sonarykheang95@gmail.com>
2025-05-29 16:17:43 +0700

    created class review #9

--- src/models/Review.ts
+++ src/models/Review.ts
@@ -4,14 +4,14 @@ export class Review{
     memberId : string;
     bookId: string;
     rating: number;
-    comment: string;
+ 
 
     constructor(reviewId: string,memberId: string, bookId: string, rating:number, comment: string){
         this.reviewId = reviewId;
         this.memberId = memberId;
         this.bookId = bookId;
         this.rating = rating;
-        this.comment = comment
+        
 
     }
 
--- src/models/enum/BookFormat.ts
+++ src/models/enum/BookFormat.ts
@@ -1,5 +1,4 @@
 export enum BookFormat{
     PHYSICAL = "physical",
     DIGITAL  = "digital",
-
 }
