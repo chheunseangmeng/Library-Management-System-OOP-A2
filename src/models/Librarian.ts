6e23a1e4dda6b28edc52fa63b2e47ce5f311c723
chheunseangmeng <seangmeng.chheun@student.passerellesnumeriques.org>
2025-06-02 15:59:39 +0700

    Fix: Updated Librarian class with registerBook functionality

--- src/models/Librarian.ts
+++ src/models/Librarian.ts
@@ -1,17 +1,11 @@
-import { Book } from "./Book";
-
 export class Librarian{
-    public book:Book[] = [];
-    public librarian:string;
-    public name:string;
-    public email:string;
+    public librarian:string
+    public name:string
+    public email:string
 
     constructor(librarian:string, name:string, email:string) {
         this.librarian = librarian;
         this.name = name;
         this.email = email;
     }
-
-    registerBook(book: Book):void{}
-    
 }
